e8791c5bca440f2c976e33764b54decf49adf336
Michael Njoku <mnjoku@mceesolutions.com>
2020-03-09 12:43:25 +0100

    stock account update

--- sunray/models/stock.py
+++ sunray/models/stock.py
@@ -2926,6 +2926,33 @@ class StockMove(models.Model):
             acc_dest = accounts_data['stock_output'].id
         self.account_id = acc_dest
         
+    def _create_account_move_line(self, credit_account_id, debit_account_id, journal_id):
+        self.ensure_one()
+        AccountMove = self.env['account.move']
+        quantity = self.env.context.get('forced_quantity', self.product_qty)
+        quantity = quantity if self._is_in() else -1 * quantity
+
+        # Make an informative `ref` on the created account move to differentiate between classic
+        # movements, vacuum and edition of past moves.
+        ref = self.picking_id.name
+        if self.env.context.get('force_valuation_amount'):
+            if self.env.context.get('forced_quantity') == 0:
+                ref = 'Revaluation of %s (negative inventory)' % ref
+            elif self.env.context.get('forced_quantity') is not None:
+                ref = 'Correction of %s (modification of past move)' % ref
+
+        move_lines = self.with_context(forced_ref=ref)._prepare_account_move_line(quantity, abs(self.value), credit_account_id, debit_account_id)
+        if move_lines:
+            date = self._context.get('force_period_date', fields.Date.context_today(self))
+            new_account_move = AccountMove.sudo().create({
+                'journal_id': journal_id,
+                'line_ids': move_lines,
+                'date': date,
+                'ref': ref,
+                'stock_move_id': self.id,
+            })
+#             new_account_move.post()
+        
     @api.multi
     def _get_accounting_data_for_valuation(self):
         """ Return the accounts and journal to use to post Journal Entries for
