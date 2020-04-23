89dea8539d223d27169e5c67a61b08bcedb398d0
Michael Njoku <mnjoku@mceesolutions.com>
2020-04-23 10:47:30 +0100

    allow manual value for return

--- sunray/models/stock.py
+++ sunray/models/stock.py
@@ -203,10 +203,10 @@ class Partners(models.Model):
     
     @api.multi
     def _check_customer_code(self, vals):
-        if  vals['supplier'] == False:
-            customer_code = self.env['res.partner'].search([('parent_account_number','=',vals['parent_account_number'])])
-            if customer_code:
-                raise UserError(_('Customer Code Already Exists!'))
+#         if  vals['supplier'] == False:
+#             customer_code = self.env['res.partner'].search([('parent_account_number','=',vals['parent_account_number'])])
+#             if customer_code:
+#                 raise UserError(_('Customer Code Already Exists!'))
     
     @api.model
     def create(self, vals):
